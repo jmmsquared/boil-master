11e58274bdc0f2529a6866bba4189e1a3038d126
jmmsquared <92335618+jmmsquared@users.noreply.github.com>
2026-07-07 11:44:11 -0500

    Update sw.js
    fixing absolute paths to fix GitHub pages deployment

--- sw.js
+++ sw.js
@@ -1,10 +1,9 @@
 const CACHE = 'boil-master-v1';
 const ASSETS = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/icons/icon-192.png',
-  '/icons/icon-512.png'
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
 ];
 
 self.addEventListener('install', e => {
